c759987df35206ab245236fe03fc3986f5a1656e
salmarami <sara.salma2014@outlook.fr>
2023-06-15 13:31:34 -0700

    c commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, q, 59);
+	return (1);
+}
